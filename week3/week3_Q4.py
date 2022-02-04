83378429093d6ecfb1b18eb43d9bb0b4582567e9
elevenfofiv <elevenfofiv@outlook.com>
2022-02-05 01:12:42 +0900

    0205_gradient descent

---
+++ week3/week3_Q4.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+beta_gd = np.random.random_sample(size=1)
+bias = np.random.random_sample(size=1)
+
+print(beta_gd, bias)
---
+++ week3/week3_Q5.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+xy = np.array([[1., 2., 3., 4., 5., 6.], [3., 6., 9., 12., 15., 18]])
+
+x_train = xy[0]
+y_train = xy[1]
+
+beta_gd = np.random.random_sample(size=1)
+bias = np.random.random_sample(size=1)
+
+learning_rate = 0.01
+
+print(x_train, y_train, beta_gd, bias)
+print(x_train.shape, y_train.shape, beta_gd.shape, bias.shape)
+
+for i in range(1001):
+    y_pred = x_train * beta_gd + bias
+    error = ((y_pred - y_train) ** 2).mean()
+
+    beta_gd -= learning_rate * ((y_pred - y_train) * x_train).mean()
+    bias -= learning_rate * ((y_pred - y_train)).mean()
+
+    if i % 100 == 0:
+        print('Epoch ({:10d}/1000) Error: {:10f}, beta_gd: {:10f}, bias: {:10f}'.format(i, error, beta_gd.item(), bias.item()))
+
