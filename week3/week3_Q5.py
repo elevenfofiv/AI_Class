4212ba4ecd367dd40e353808f9cad0e06503eee7
elevenfofiv <elevenfofiv@outlook.com>
2022-02-06 14:07:07 +0900

    0206

--- week3/week3_Q5.py
+++ week3/week3_Q5.py
@@ -8,7 +8,7 @@ y_train = xy[1]
 beta_gd = np.random.random_sample(size=1)
 bias = np.random.random_sample(size=1)
 
-learning_rate = 0.01
+learning_rate = 0.05
 
 print(x_train, y_train, beta_gd, bias)
 print(x_train.shape, y_train.shape, beta_gd.shape, bias.shape)
@@ -17,8 +17,8 @@ for i in range(1001):
     y_pred = x_train * beta_gd + bias
     error = ((y_pred - y_train) ** 2).mean()
 
-    beta_gd -= learning_rate * ((y_pred - y_train) * x_train).mean()
-    bias -= learning_rate * ((y_pred - y_train)).mean()
+    beta_gd -= learning_rate * ((y_pred - y_train) * 2 * x_train).mean()
+    bias -= learning_rate * ((y_pred - y_train) * 2).mean()
 
     if i % 100 == 0:
         print('Epoch ({:10d}/1000) Error: {:10f}, beta_gd: {:10f}, bias: {:10f}'.format(i, error, beta_gd.item(), bias.item()))
