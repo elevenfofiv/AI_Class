aa7173867621d79d6f871ecd92b9076f7ed3829e
elevenfofiv <elevenfofiv@outlook.com>
2022-02-11 22:30:13 +0900

    0211

--- try.py
+++ try.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 xy = np.array([[1., 2., 3., 4., 5., 6.], [3., 6., 9., 12., 15., 18]])
 
@@ -10,18 +11,16 @@ bias = np.random.random_sample(size=1)
 
 learning_rate = 0.01
 
-print(x_train, y_train, beta_gd, bias)
-print(x_train.shape, y_train.shape, beta_gd.shape, bias.shape)
-
 for i in range(1001):
     y_pred = x_train * beta_gd + bias
-    error = ((y_pred - y_train) ** 2).mean()
+    error = tf.keras.losses.mse(y_pred, y_train).numpy()
+    # print(error)
+    grad_beta_gd = (tf.reduce_mean((y_pred - y_train) * 2 * x_train)).numpy()
+    grad_bias = (tf.reduce_mean((y_pred - y_train) * 2)).numpy()
+    # print(grad_beta_gd, grad_bias)
 
-    grad_w = ((y_pred - y_train) * 2 * x_train).mean()
-    grad_b = ((y_pred - y_train) * 2).mean()
-
-    beta_gd -= learning_rate * grad_w
-    bias -= learning_rate * grad_b
+    beta_gd -= learning_rate * grad_beta_gd
+    bias -= learning_rate * grad_bias
 
     if i % 100 == 0:
         print('Epoch ({:10d}/1000) Error: {:10f}, beta_gd: {:10f}, bias: {:10f}'.format(i, error, beta_gd.item(), bias.item()))
